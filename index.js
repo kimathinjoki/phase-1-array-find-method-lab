21870837ae612a6759509764c5c58fac3b6389c4
kimathinjoki <dfkimathinjoki@gmail.com>
2022-11-28 15:51:28 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+  ]
+
+
+  function superbowlWin(array){
+    for(const item of array){
+        if (item.result === "W"){
+            return item.year
+        }
+
+    }
+  }
+
+  console.log(record.find(superbowlWin(record)));
